c79f83b493d08fcd215d654cfa6a1f40a3b7d2a6
khinkyaw1011 <khintheigar1@gmail.com>
2022-11-04 20:40:03 +0630

    update

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ export function showMyName(){
 export function showMyNameUI(){
     const h1=document.createElement('h1');
     h1.innerHTML=myname;
-    h1.style.position='fixed';
+    h1.style.position="fixed";
     h1.style.zIndex=1000;
-    h1.style.fontSize=100+'px';
+    h1.style.fontSize=100+"px";
     h1.style.top=0;
     h1.style.right=0;
     h1.style.backgroundColor="black";
